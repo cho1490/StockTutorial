208e26be020ba107feab4d09a24132de40ff1e34
cho1490 <chosanghyun99@gmail.com>
2021-07-28 01:08:22 +0900

    first commit

---
+++ StockTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockTutorial
+//
+//  Created by 조상현 on 2021/07/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
